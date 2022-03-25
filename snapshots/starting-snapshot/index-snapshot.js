8fcf453e9ff96712d75b139ab0d3ba02c6f45049
winkingcat <dankercher@gmail.com>
2022-03-25 10:30:42 -0400

    init

--- snapshots/starting-snapshot/index-snapshot.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App-snapshot';
-
-ReactDOM.render(<App />, document.getElementById('root'));
-
