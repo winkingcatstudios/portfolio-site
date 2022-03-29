e01cbe12d4286efb690ec87a3a58435553e89460
winkingcat <dankercher@gmail.com>
2022-03-29 17:53:30 -0400

    loadingspinner and react lazy loading

--- src/App.js
+++ src/App.js
@@ -1,36 +1,51 @@
+import React, { Suspense } from "react";
 import { Route, Switch } from "react-router-dom";
 import AboutPage from "./pages/About";
-import SkillsPage from "./pages/Skills";
-import ProjectsPage from "./pages/Projects";
-import FavoritesPage from "./pages/Favorites";
-import ContactPage from "./pages/Contact";
+// import SkillsPage from "./pages/Skills";
+// import ProjectsPage from "./pages/Projects";
+// import FavoritesPage from "./pages/Favorites";
+// import ContactPage from "./pages/Contact";
 import Layout from "./components/layout/Layout";
 import Footer from "./components/layout/Footer";
+import LoadingSpinner from "./components/ui/LoadingSpinner";
+
+const SkillsPage = React.lazy(() => import("./pages/Skills"));
+const ProjectsPage = React.lazy(() => import("./pages/Projects"));
+const FavoritesPage = React.lazy(() => import("./pages/Favorites"));
+const ContactPage = React.lazy(() => import("./pages/Contact"));
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path="/" exact>
-          <AboutPage />
-          <Footer />
-        </Route>
-        <Route path="/skills">
-          <SkillsPage />
-          <Footer />
-        </Route>
-        <Route path="/projects">
-          <ProjectsPage />
-          <Footer />
-        </Route>
-        <Route path="/favorites">
-          <FavoritesPage />
-          <Footer />
-        </Route>
-        <Route path="/contact">
-          <ContactPage />
-        </Route>
-      </Switch>
+      <Suspense
+        fallback={
+          <div className="centered">
+            <LoadingSpinner />
+          </div>
+        }
+      >
+        <Switch>
+          <Route path="/" exact>
+            <AboutPage />
+            <Footer />
+          </Route>
+          <Route path="/skills">
+            <SkillsPage />
+            <Footer />
+          </Route>
+          <Route path="/projects">
+            <ProjectsPage />
+            <Footer />
+          </Route>
+          <Route path="/favorites">
+            <FavoritesPage />
+            <Footer />
+          </Route>
+          <Route path="/contact">
+            <ContactPage />
+          </Route>
+        </Switch>
+      </Suspense>
     </Layout>
   );
 }
